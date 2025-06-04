f23d9f564997058b631a8b9c3c1d5a140b6b3bcb
Arthur de Souza <arthursbezerra7306@gmail.com>
2025-06-04 16:44:06 -0300

    Update soma2.asm

--- soma2.asm
+++ soma2.asm
@@ -2,19 +2,16 @@
 ; Programa: Soma  com input
 ; Autor: Arthur de Souza
 ; Data: 04.06.2025
-; Recomendo usar o "passo a passo", não descobri como fazer com que o Neander pare 
-; para o 2° input ser colocado :(
 ;---------------------------------------------------
-IN 00 ; Input do primeiro número (Utiliza-se as chaves)
-STA 20 ; Armazena no endereço 20 (14 em Hex, ali do lado)
+IN 00 ; Input do primeiro nÃºmero (Utiliza-se as chaves)
+STA 20 ; Armazena no endereÃ§o 20 (14 em Hex, ali do lado)
 
-IN 00 ; Input do segundo número
-STA 21 ; Armazena no endereço 21 (15 Hex)
+IN 00 ; Input do segundo nÃºmero
+STA 21 ; Armazena no endereÃ§o 21 (15 Hex)
 
 SOMA:
-LDA 20 ; Carrega no acumulador o conteúdo do endereço 20 (Primeiro número)
-ADD 21 ; Adiciona o conteúdo do endereço 21 (Segundo número) no acumulador
-STA 22 ; Armazena o resultado no endereço 22 (16 Hex)
+LDA 20 ; Carrega no acumulador o conteÃºdo do endereÃ§o 20 (Primeiro nÃºmero)
+ADD 21 ; Adiciona o conteÃºdo do endereÃ§o 21 (Segundo nÃºmero) no acumulador
+STA 22 ; Armazena o resultado no endereÃ§o 22 (16 Hex)
 OUT 00 ; Mostra o resultado no visor (em Hex)
-HLT ; Para a máquina
-
+HLT ; Para a mÃ¡quina
